53d6c2b48ee462b2d2eaefd5f06fb2b0d4d9fe79
Ohara Kazuhiro <57878514+kazuhe@users.noreply.github.com>
2023-02-08 01:33:54 +0900

    Fix #7 (#8)
    * NodeController をリファクタリング
    
    * NodeService と NodeServiceImpl を作成し利用する

--- src/main/java/x/plantree/controllers/NodeController.java
+++ src/main/java/x/plantree/controllers/NodeController.java
@@ -1,39 +1,43 @@
 package x.plantree.controllers;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import x.plantree.models.Node;
+import x.plantree.services.NodeService;
 
 @RestController
 @RequestMapping(path = NodeController.BASE_URL)
 public class NodeController {
   public static final String BASE_URL = "/api/v1/nodes";
 
-  private final AtomicInteger counter = new AtomicInteger();
-
-  private List<Node> nodeItems = new ArrayList<>();
+  @Autowired
+  private NodeService nodeService;
 
   /**
-   * ID を用いてインメモリオブジェクトから Node を取得する
+   * Node を作成する
    * 
-   * @param id Node ID
-   * @return Node
+   * @param newNode 新しい Node
+   * @return 作成された Node
    */
-  private Node getNodeById(int id) {
-    return nodeItems.stream().filter(item -> item.getId() == id).findAny().orElse(null);
+  @PostMapping(path = "")
+  public ResponseEntity<Node> createNode(@RequestBody Node newNode) {
+    Node savedNode = nodeService.saveNode(newNode);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(savedNode.getId()).toUri();
+    return ResponseEntity.created(location).body(savedNode);
   }
 
   /**
@@ -41,9 +45,10 @@ public class NodeController {
    * 
    * @return Node 一覧
    */
-  @RequestMapping(method = RequestMethod.GET, path = "")
-  public List<Node> getNodeItems() {
-    return nodeItems;
+  @GetMapping(path = "")
+  public ResponseEntity<List<Node>> getNodeList() {
+    List<Node> nodeList = nodeService.getNodeList();
+    return ResponseEntity.ok(nodeList);
   }
 
   /**
@@ -52,29 +57,10 @@ public class NodeController {
    * @param id Node ID
    * @return Node
    */
-  @RequestMapping(method = RequestMethod.GET, path = "/{id}")
-  public Node getNodeItem(@PathVariable int id) {
-    Node result = getNodeById(id);
-    if (result == null) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
-    }
-
-    return result;
-  }
-
-  /**
-   * Node を作成する
-   * 
-   * @param newNode 新しい Node
-   * @return 作成された Node
-   */
-  @RequestMapping(method = RequestMethod.POST, path = "")
-  public ResponseEntity<Node> createNodeItem(@RequestBody Node newNode) {
-    newNode.setId(counter.incrementAndGet());
-    nodeItems.add(newNode);
-    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newNode.getId())
-        .toUri();
-    return ResponseEntity.created(location).body(newNode);
+  @GetMapping(path = "/{id}")
+  public ResponseEntity<Node> getNode(@PathVariable int id) {
+    Node node = nodeService.getNodeById(id);
+    return ResponseEntity.ok(node);
   }
 
   /**
@@ -83,16 +69,9 @@ public class NodeController {
    * @param newNode 新しい Node
    * @param id      新しい Node ID
    */
-  @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
-  public ResponseEntity<?> updateNodeItem(@RequestBody Node newNode, @PathVariable int id) {
-    Node result = getNodeById(id);
-    if (result == null) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
-    }
-
-    nodeItems.remove(result);
-    nodeItems.add(newNode);
-
+  @PutMapping(path = "/{id}")
+  public ResponseEntity<?> updateNode(@RequestBody Node newNode, @PathVariable int id) {
+    nodeService.updateNode(id, newNode);
     return ResponseEntity.noContent().build();
   }
 
@@ -101,14 +80,9 @@ public class NodeController {
    * 
    * @param id Node ID
    */
-  @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+  @DeleteMapping(path = "/{id}")
   public ResponseEntity<?> removeNodeItem(@PathVariable int id) {
-    Node result = getNodeById(id);
-    if (result == null) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
-    }
-    nodeItems.remove(result);
-
+    nodeService.deleteNode(id);
     return ResponseEntity.noContent().build();
   }
 }
---
+++ src/main/java/x/plantree/services/NodeServiceImpl.java
@@ -0,0 +1,69 @@
+package x.plantree.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import x.plantree.models.Node;
+
+@Service
+public class NodeServiceImpl implements NodeService {
+
+  private final AtomicInteger counter = new AtomicInteger();
+
+  private List<Node> nodeList = new ArrayList<>();
+
+  /**
+   * ID を用いてインメモリオブジェクトから Node を取得する
+   * 
+   * @param id Node ID
+   * @return Node
+   */
+  private Node findNodeById(int id) {
+    Optional<Node> result = nodeList.stream().filter(item -> item.getId() == id).findAny();
+    if (!result.isPresent()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
+    }
+
+    return result.get();
+  }
+
+  @Override
+  public Node saveNode(Node node) {
+    node.setId(counter.incrementAndGet());
+    nodeList.add(node);
+
+    return node;
+  }
+
+  @Override
+  public List<Node> getNodeList() {
+    return nodeList;
+  }
+
+  @Override
+  public Node getNodeById(int id) {
+    return findNodeById(id);
+  }
+
+  @Override
+  public Node updateNode(int id, Node node) {
+    Node result = findNodeById(id);
+    nodeList.remove(result);
+    nodeList.add(node);
+
+    return node;
+  }
+
+  @Override
+  public void deleteNode(int id) {
+    Node result = findNodeById(id);
+    nodeList.remove(result);
+  }
+
+}
