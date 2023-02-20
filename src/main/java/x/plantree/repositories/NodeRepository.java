53e648d8f5ad2375ddbab4ae9212a16ff29701d9
kazuhe <omc.ohara@gmail.com>
2023-02-20 23:04:35 +0900

    Spring Data JPA から Spring JDBC に変更

--- src/main/java/x/plantree/repositories/NodeRepository.java
+++ src/main/java/x/plantree/repositories/NodeRepository.java
@@ -1,10 +1,61 @@
 package x.plantree.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import x.plantree.models.Node;
 
 @Repository
-public interface NodeRepository extends JpaRepository<Node, Integer> {
+public class NodeRepository {
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  public List<Node> findAll() {
+    String sql = "SELECT * FROM nodes";
+    SqlRowSet rs = jdbcTemplate.queryForRowSet(sql);
+    List<Node> list = new ArrayList<>();
+    while (rs.next()) {
+      list.add(new Node(rs.getInt("id"), rs.getString("title")));
+    }
+
+    return list;
+  }
+
+  // ↓WIP
+
+  private List<Node> nodeList = new ArrayList<>();
+
+  /**
+   * ID を用いてインメモリオブジェクトから Node を取得する
+   * 
+   * @param id Node ID
+   * @return Node
+   */
+  private Node findNodeById(int id) {
+    Optional<Node> result = nodeList.stream().filter(item -> item.getId() == id).findAny();
+
+    return result.get();
+  }
+
+  public Node save(Node node) {
+    nodeList.add(node);
+
+    return node;
+  }
+
+  public Optional<Node> findById(int id) {
+    return nodeList.stream().filter(item -> item.getId() == id).findAny();
+  }
+
+  public void deleteById(int id) {
+    Node result = findNodeById(id);
+    nodeList.remove(result);
+  }
 }
