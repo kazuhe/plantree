5ee04a719ff0bf8d988c1e53e7520296d28b05f8
kazuhe <omc.ohara@gmail.com>
2023-04-18 00:16:56 +0900

    Kotlin に変更

--- docker/db/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dev.nodes
-(
-    id INTEGER NOT NULL,
-    title VARCHAR(32) NOT NULL,
-    PRIMARY KEY (id)
-);
---
+++ src/main/kotlin/x/plantree/PlantreeApplication.kt
@@ -0,0 +1,11 @@
+package x.plantree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlantreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlantreeApplication>(*args)
+}
